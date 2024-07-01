408863a5688a6421c7c8629335598dba5450997c
Himasnhu Kumar <himanshukumarmpr09@gamil.com>
2024-07-01 12:53:37 +0530

    This is js

---
+++ index.js
@@ -0,0 +1,14 @@
+// Create a button element
+// var button = document.createElement('button');
+// Set button attributes and content-0;
+// button.id = ''; // Set the id attribute
+// button.className = 'btn'; // Set the class attribute
+// button.innerHTML = 'Click me'; // Set the inner text
+// button.onclick = function() {
+//     alert('Button clicked!');
+ // Add more functionality here if needed
+// };
+// events 
+
+
+
